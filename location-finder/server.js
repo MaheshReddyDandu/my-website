7b17a597c7e5c9a4264cbba9eb7915302e099227
MaheshReddyDandu <contact@maheshreddy.in>
2025-03-29 13:10:48 +0530

    maha

--- location-finder/server.js
+++ location-finder/server.js
@@ -61,7 +61,8 @@ app.get('/api/calculate-distance', async (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const server = app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 process.on('SIGINT', () => {
     console.log('🛑 Server shutting down gracefully...');
